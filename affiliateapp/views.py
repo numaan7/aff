38babe460c8ce42970f524d46153dde85075a2a5
Ankur Kumar <bitcoinwallet02525@gmail.com>
2023-03-05 10:11:10 +0530

    removed print statement

--- affiliateapp/views.py
+++ affiliateapp/views.py
@@ -4,7 +4,6 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()    
-    print("--------------------",products[0].image)
 
     return render(request, 'index.html', context={'products': products})
 
