978bab3c615573e30ec8ee37a78c6f84fb148376
Ankur Kumar <bitcoinwallet02525@gmail.com>
2023-03-10 22:08:19 +0530

    added images

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -128,7 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print("====================", BASE_DIR)
 
 
 # Default primary key field type
