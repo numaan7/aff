272065b1f7a73bf2b7973ab50b1d3e5bd68302ce
Ankur Kumar <bitcoinwallet02525@gmail.com>
2023-03-12 01:02:04 +0530

    debug is equl to false

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zdch(9a=$f+y41yu)uin8lu@!d=h83igr=7*v&p749dm9cke1p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
